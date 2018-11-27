edbe2819fb10574f28c2b1dcee926e874794b40e
ganeshlohiya <43986241+ganeshlohiya@users.noreply.github.com>
2018-11-27 15:04:47 +0530

    Update temp_scale.py

--- temp_scale.py
+++ temp_scale.py
@@ -1,8 +1,12 @@
-x=int(input("Enter temperature:"))
-y=input("is it in celcius or fahrenhite")
+# Title : Temperature Scale
+# Ganesh lohiya
+# M 36
+
+x=int(input("Enter temperature:"))  # take input from the user
+y=input("is it in celcius or fahrenhite")  # ask for the unit of temperature
 if y is "celcius": 
- f=(9*x/5)+32 
+ f=(9*x/5)+32   # celcius to fahrenhite formula
  print(f)
 else: 
- c=(5/9)*(x-32)
+ c=(5/9)*(x-32)   # fahrenhite to celsius formula
  print(c)
