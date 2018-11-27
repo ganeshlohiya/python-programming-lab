3441ee3bcea93036855f7d761a1bb23e8e1e554d
ganeshlohiya <43986241+ganeshlohiya@users.noreply.github.com>
2018-11-27 15:01:02 +0530

    Update random_dice.py

--- random_dice.py
+++ random_dice.py
@@ -1,13 +1,18 @@
+# title : Dice Roll
+# Ganesh Lohiya
+# M 36
+
+
 import random
-game_start = input("Would you like to roll the dice?") 
-def dice_roll():
- print("Your number is: " + str(random.randint(1,6)))
+game_start = input("Would you like to roll the dice?")   # take input from the user
+def dice_roll():  # define a function for rolling a dice
+ print("Your number is: " + str(random.randint(1,6)))   #using random function function, it will pick any of the six numbers
  global play_again 
- play_again = input("Would you like to play again?") 
+ play_again = input("Would you like to play again?")   # if user wants to play again
 if game_start == "yes":
  dice_roll() 
  while play_again == "yes":
   dice_roll()
-elif game_start == "no":
+elif game_start == "no": # the game will continue to roll the dice until you input 'no' or some other input
  print("Game Over")
 else: print("Input not recognized")
