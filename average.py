ba8d6575f47d0b71abee8ff5412e7630b233c88d
ganeshlohiya <43986241+ganeshlohiya@users.noreply.github.com>
2018-11-27 14:33:51 +0530

    Update average.py

--- average.py
+++ average.py
@@ -1,9 +1,13 @@
+# Title: calculate average
+# Ganesh Lohiya 
+# M 36
+
 total=0
 n=0
 count=0
-while n>=0:
-  total+=n
-  count+=1
-  n=int(input())
-avg=total/(count-1)
+while n>=0:       # while loop
+  total+=n        # total of all the integers 
+  count+=1        # for no of integers added
+  n=int(input())  # taking input in integer type
+avg=total/(count-1)    # average formula
 print(avg)
