8fb59c20c8f129458e698c1ad9a85efc36084694
ganeshlohiya <43986241+ganeshlohiya@users.noreply.github.com>
2018-11-27 14:43:48 +0530

    Update fibonaki.py

--- fibonaki.py
+++ fibonaki.py
@@ -1,20 +1,24 @@
-def fibonaki(n):
+# Title : Fibonaki Series
+# Ganesh Lohiya
+# M 36
+
+def fibonaki(n):   #defining a function 
  a=0
  b=1
- l=[]
+ l=[]            #introducing blank list
  if n<0:
-  print("enter positive number")
+  print("enter positive number")     # fibonaki series cannot be calculated for negative number
  elif n==0:
-  print('0')
+  print('0')  
  elif n==1:
   print('0,1')
  else:
-  l.append(a)
+  l.append(a)  # append a and b in list l
   l.append(b)
   for i in range (n-2):
-   f=a+b
-   l.append(f)
-   a=b
+   f=a+b #fibonaki series logic
+   l.append(f) # append all the numbers in the list till the the loop exits
+   a=b  #let each number take its next value
    b=f
   print(l)
 a=int(input())
