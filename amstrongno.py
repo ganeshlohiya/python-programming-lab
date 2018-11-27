4c0bab5f58c8ef69f009480e393d4571825a7464
ganeshlohiya <43986241+ganeshlohiya@users.noreply.github.com>
2018-11-27 13:13:23 +0530

    Update amstrongno.py

--- amstrongno.py
+++ amstrongno.py
@@ -1,14 +1,14 @@
 def armstrong_no(x):
  s=0
- z=x
+ z=x      #save x in z
  while x>0:
-  y=x%10
-  s+=y**3
-  x=x//10
- print(s)
+  y=x%10     #y is stored as remainder of x when divided by 10
+  s+=y**3     #take cube of remainder and add it to s
+  x=x//10      #x is now stored as quetiont of x after dividing by 10
+ print(s)      #repeat this process till last digit
  if s==z:
-  print("armstrong number")
+  print("Armstrong number")     #if final x is same as z 
  else:
-  print("not an armstrong number")
+  print("not an armstrong number")    # if not
 b=int(input())
 armstrong_no(b)
