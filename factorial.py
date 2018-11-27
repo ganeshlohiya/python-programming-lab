59a1bb157063fa54a2aef664aa7c4d3de984264e
ganeshlohiya <43986241+ganeshlohiya@users.noreply.github.com>
2018-11-27 14:39:02 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,13 +1,17 @@
-def factorial(n):
+# title : Factorial of given number
+# Ganesh Lohiya
+# M 36
+
+def factorial(n):    # defining a function 
  f=1
  if n<0:
-  print("enter positive number")
+  print("enter positive number")    # factorial can not be calculated for negative no.
  elif n==0 or n==1:
-  print('1')
+  print('1')    # factorial of 0 and 1 is 1
  else:  
   while n>1:
-   f=f*n
-   n=n-1
+   f=f*n     # for multiplying all the numbers
+   n=n-1     # bringing each value
   print(f)
 x=int(input())
 factorial(x)
